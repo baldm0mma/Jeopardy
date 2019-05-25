6b25d64a982933e2b4c1fa638716077eef7fd0d2
Jev Forsberg <jev.forsberg@gmail.com>
2019-05-25 11:13:50 -0600

    All testing updated, but spies not functioning
    Co-authored-by: Hindreen Fawzi <hndfaw@gmail.com>
    Co-authored-by: DeMarcus Kirby <tkd024@gmail.com>
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

--- test/Round.test.js
+++ test/Round.test.js
@@ -1,13 +1,19 @@
 import chai from 'chai';
 const expect = chai.expect;
+import spies from 'chai-spies';
+chai.use(spies);
 import Round from '../src/Round';
 import data from "../src/dataset";
+import domUpdates from "..src/domUpdates";
+// chai.spy.on(domUpdates, 'updateScore', () => true);
+// chai.spy.on(domUpdates, 'turnPrompt', () => true);
 
 describe('Round', function() {
 
   let round;
   let allPlayers;
   let clue;
+
   beforeEach(function() {
     round = new Round([1, 2, 3, 4], allPlayers, data, 1);
     allPlayers = [{ name: 'jev', id: 1, score: 0 }, 
@@ -19,6 +25,7 @@ describe('Round', function() {
       answer: "Six Feet Under",
       categoryId: 10
     }
+
   });
 
   it('should be a function', function() {
@@ -33,20 +40,20 @@ describe('Round', function() {
     expect(round.allPlayers).to.have.lengthOf(3);
   });
 
-  it('should have a turn number that begins with 1', function() {
-    expect(round.roundNumber).to.equal(1);
+  it('should have a current turn number that begins with 1', function() {
+    expect(round.currentTurn).to.equal(1);
   });
 
   it('should have a round number that begins with 1', function() {
     expect(round.roundNumber).to.equal(1);
   });
 
-  it('should have a current turn number that begins with 1', function() {
-    expect(round.currentTurn).to.equal(1);
+  it('should have a question counter that begins at 16', function() {
+    expect(round.questionCounter).to.equal(16);
   });
 
   it('should have a category titles list that are the 4 categories currently being played returned in plain English', function() {
-    expect(round.categoryTitles).to.have.lengthOf(4);
+    expect(round.categoryTitles).to.eql([ 'United States History', 'Life Sciences', 'Public Health', 'Education Jargon' ]);
   });
 
   it('should have a category clues list that are 4 clues from each of the 4 categories', function() {
@@ -57,18 +64,32 @@ describe('Round', function() {
     expect(round.confirmCurrentPlayer()).to.eql({ name: 'jev', id: 1, score: 0 });
   });
 
+  it('should have a method that increments the current turn counter', function() {
+    expect(round.currentTurn).to.equal(1);
+    round.incrementNextTurn();
+    expect(round.currentTurn).to.equal(2);
+  });
+
+  it('should have a method that returns the point value to be incremented or decremented', function() {
+    expect(round.calculateRegularPoints(clue)).to.eql(300);
+  });
+
   it('should have a method that validates the current answer, and updates points accordingly', function() {
+    expect(domUpdates.updateScore).to.have.been.called(1);
+    expect(domUpdates.turnPrompt).to.have.been.called(1);
+
     expect(round.allPlayers[0].score).to.equal(0);
     round.validateCurrentAnswer("Six Feet Under", clue, [1, 1]);
     expect(round.allPlayers[0].score).to.equal(300);
 
-    round.validateCurrentAnswer("I dunno", clue), [1, 1];
+    round.validateCurrentAnswer("I dunno", clue, [1, 1]);
     expect(round.allPlayers[0].score).to.equal(0);
   });
 
-  it('should have a method that returns a random array of numbers, 0-3, with which to target the current round\'s daily double', function() {
+  it('should have a method that returns a random array of numbers, 0-3, with which to target/assign the current round\'s daily double(s)', function() {
     expect(round.generateDailyDoublePosition()).to.have.lengthOf(2);
     let round2 = new Round([1, 2, 3, 4], allPlayers, data, 2);
     expect(round2.generateDailyDoublePosition()).to.have.lengthOf(4);
   });
+
 });
